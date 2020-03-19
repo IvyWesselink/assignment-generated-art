f4aa2b42ddf62359581bb3e3b08a48f6cb22fd68
IvyWesselink <55988292+IvyWesselink@users.noreply.github.com>
2020-03-19 12:43:25 +0100

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,40 +1,26 @@
 let xmlns = "http://www.w3.org/2000/svg";
 let svg = document.createElementNS(xmlns, "svg");
-svg.setAttribute("width", 1000);
-svg.setAttribute("height", 1000);
-svg.setAttribute("viewbox", '0 0 1000 1000');
+svg.setAttribute("width", 500);
+svg.setAttribute("height", 500);
+svg.setAttribute("viewbox", '0 0 500 500');
 
-const arrayX = [50, 100, 150, 200, 250, 300, 350, 400, 450, 500];
-const arrayY = [100, 200, 300, 400, 500, 600];
 
-let count = 0;
-let fill = 255 / (arrayX.length * arrayY.length);
-for (j = 0; j < arrayY.length; j++) {
-  for (i = 0; i < arrayX.length; i++) {
-    if (i == 0) {
+let arrKleur = ["#503D3F", "#615756", "#539987", "#52FFB8", "#4DFFF3"];
 
-    let rect = document.createElementNS(xmlns, "rect");
-    rect.setAttribute("x", arrayX[i] - 10);
-    rect.setAttribute("y", arrayY[j] - 10);
-    rect.setAttribute("width", 20);
-    rect.setAttribute("height", 20);
-    rect.setAttribute("fill", `rgb(${255 - (fill * count)},0,0)`);
+for(i=0;i<1200;i++){
+    const randomIndex = Math.floor(Math.random()*4);
 
-    svg.appendChild(rect);
+    let circleRandom = document.createElementNS(xmlns, "circle");
+    circleRandom.setAttribute("cx", Math.floor(Math.random()*500) + 1);
+    circleRandom.setAttribute("cy", Math.floor(Math.random()*500) + 1);
+    circleRandom.setAttribute("r", Math.floor(Math.random()*15) + 3);
+    circleRandom.setAttribute("fill", arrKleur[randomIndex]);
+    
+    svg.appendChild(circleRandom);
+    document.body.appendChild(svg);
+};
 
-    } else {
 
-        let circle= document.createElementNS(xmlns, "circle");
-        circle.setAttribute("cx", arrayX[i]);
-        circle.setAttribute("cy", arrayY[j]);
-        circle.setAttribute("r", 10);
-        circle.setAttribute("fill", `rgb(${255 - (fill * count)},0,0)`);
 
-        svg.appendChild(circle);
-    }
 
-    count++;
-  }
-}
 
-document.body.appendChild(svg);
